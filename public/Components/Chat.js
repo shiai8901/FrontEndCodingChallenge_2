009e1ad1b5015c5fba79f99bce45eb7defc1ac7b
Ai Shi <shiai8901@gmail.com>
2017-06-15 17:59:28 -0700

    added get request for messages

--- public/Components/Chat.js
+++ public/Components/Chat.js
@@ -9,17 +9,26 @@ export default class Chat extends React.Component {
 	constructor(props) {
 		super(props); 
 		this.state = {
-			currentRoom: ""
+			currentRoom: "",
+			currentRoomId: ""
 		}
 		this.handleChangeRoom = this.handleChangeRoom.bind(this);
 	}
 
-	handleChangeRoom(roomname) {
-		console.log(roomname);
-		this.setState({currentRoom: roomname});
-		axios.get('http://localhost:8080/api/rooms/0')
+	handleChangeRoom(id) {
+		console.log(id);
+		// this.setState({currentRoom: id});
+		console.log('http://localhost:8080/api/rooms/' + id);
+		axios.get('http://localhost:8080/api/rooms/' + id)
 			.then(res => {
-				this.setState({currentRoomUser: res.data.users});
+				this.setState({currentRoomUser: res.data.users, 
+								currentRoom: res.data.name, 
+								currentRoomId: id});
+			})
+			.catch(err => console.log(err.message));
+		axios.get("http://localhost:8080/api/rooms/" + id + "/messages")
+			.then(res => {
+				this.setState({currentRoomMessage: res.data})
 			})
 			.catch(err => console.log(err.message));
 	}
@@ -28,8 +37,14 @@ export default class Chat extends React.Component {
 		console.log('props in Chat', this.props);
 		return (
 			<div>
-				<LeftCol handleChangeRoom={this.handleChangeRoom} username={this.props.username} rooms={this.props.rooms} onlineSince={this.props.onlineSince}/>
-				<Message currentRoomUser={this.state.currentRoomUser} currentRoom={this.state.currentRoom} className="content" />
+				<LeftCol handleChangeRoom={this.handleChangeRoom} 
+						username={this.props.username} 
+						rooms={this.props.rooms} 
+						onlineSince={this.props.onlineSince}/>
+				<Message currentRoomUser={this.state.currentRoomUser} 
+						currentRoom={this.state.currentRoom} 
+						currentRoomMessage={this.state.currentRoomMessage}
+						className="content" />
 			</div>
 			)
 	}
