e730fd04254fe27c11c2ce5c589d59bd8f913a72
Ai Shi <shiai8901@gmail.com>
2017-06-15 17:29:00 -0700

    removed react router

--- public/index.js
+++ public/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 // import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import ReactDOM from 'react-dom';
-// import routes from './Components/routes';
+import { BrowserRouter, Route } from 'react-router-dom'
 import App from './Components/App';
-
+import Chat from './Components/Chat';
+import LogIn from './Components/LogIn';
+// history={browserHistory}
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+// ReactDOM.render(
+// 	(<BrowserRouter>
+// 		<div>
+// 			<Route path="/" component={App} />
+// 			<Route path="/chat" component={Chat}/>
+// 		</div>
+// 	</BrowserRouter>
+// 	)
+// 	, document.getElementById('app'));
