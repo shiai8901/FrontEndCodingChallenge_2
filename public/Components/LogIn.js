37c25fb0a3403ebb13f4a891e4c122a504695cf6
Ai Shi <shiai8901@gmail.com>
2017-06-15 17:25:53 -0700

    send get request to server for all room list when user sign in

---
+++ public/Components/LogIn.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from '../styles.css';
+// import { Redirect, Link } from 'react-router-dom';
+import Chat from './Chat';
+
+export default class LogIn extends React.Component {
+	constructor(props) {
+		super(props); 
+	}
+
+	render () {
+		console.log(this.state);
+		return (
+			<div>
+				<form id="logIn" onSubmit={this.props.submitUsername}>
+					<input id="logInInput" type="text" name="username" placeholder="Type your username..." /><br></br>
+					<button id="logInButton" type="submit" >Join the DoorDash Chat!</button>
+				</form>
+			</div>
+			)		
+	}
+}
