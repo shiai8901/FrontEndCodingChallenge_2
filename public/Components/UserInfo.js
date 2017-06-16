104a4b1ef31b1b40de50f79d873c3a1f63f842ce
Ai Shi <shiai8901@gmail.com>
2017-06-15 17:21:22 -0700

    modulized userInfo component

---
+++ public/Components/UserInfo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from '../styles.css'
+
+export default class UserInfo extends React.Component {
+	constructor(props) {
+		super(props); 
+		this.state = {
+			onlineDuration: 0
+		}
+		this.updateOnlineDuration = this.updateOnlineDuration.bind(this);
+	}
+
+	componentDidMount() {
+		this.setState({startTime: (new Date()).getTime(), onlineDuration: 0});
+		var timer;
+		setInterval(this.updateOnlineDuration, 10000);
+	}
+
+	updateOnlineDuration() {
+		var context = this;
+		this.setState({onlineDuration: context.state.onlineDuration + 1});
+	}
+
+	render() {
+		console.log('UserInfo props: ', this.props);
+		return (
+			<div>
+				<p className="userName">{this.props.username}</p>
+				<p className="onlineTime">Online for {this.state.onlineDuration} minutes</p>
+			</div>
+			)
+	}
+}
